72b5a4f9cfb3b554c758cc6545b09e45f8651d05
Mike Gensel <mike@genselfamily.com>
2025-03-01 05:38:31 +0000

    Cleanup repository structure for HACS custom component

--- src/ha_agent/models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Models package for Home Assistant Agent.
-Provides model initialization and switching capabilities.
-"""
-
-from .model_switcher import ModelSwitcher
-
-__all__ = ["ModelSwitcher"] 
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Tests package for Home Assistant Agent.
-""" 
